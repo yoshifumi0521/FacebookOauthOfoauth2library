f7ce13c096877056bb30e93ada66ea3741305893
YoshifumiSato <yoshifumi0521@gmail.com>
2012-10-13 06:46:45 +0900

    Railsアプリのインストール

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookOauthOfoauth2library::Application.config.secret_token = '57223692fafd81cb06831aab62b3a30ee3b0e00ac944f5841a9a2ddf741d153f82feaa99e9b58c8b1a41ffa6371795aa18da49f78ebc9018710027ce8a8bd548'
